2136af73799e9cbd0e9064251f4c0a2425fe0d53
ex_gaojh1 <jiahao.gao@partner.midea.com>
2018-11-06 18:25:05 +0800

    1.主页切图

--- components/public/header/nav.vue
+++ components/public/header/nav.vue
@@ -1,66 +1,90 @@
 <template>
-  <section class="container">
-    <div>
-      <logo/>
-      <h1 class="title">
-        project
-      </h1>
-      <h2 class="subtitle">
-        My good Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+    <div class="m-nav">
+      <ul class="nav">
+        <li class="list">
+          <nuxt-link to="/my">我的美团</nuxt-link>
+          <dl>
+            <dd><nuxt-link to="/my">我的订单</nuxt-link></dd>
+            <dd><nuxt-link to="/my">我的收藏</nuxt-link></dd>
+            <dd><nuxt-link to="/my">抵用券</nuxt-link></dd>
+            <dd><nuxt-link to="/my">账户设置</nuxt-link></dd>
+          </dl>
+        </li>
+        <li >
+          <nuxt-link to="/order">手机APP</nuxt-link>
+        </li>
+        <li  class="list bd">
+          <nuxt-link to="/center">商家中心</nuxt-link>
+          <dl>
+            <dd><nuxt-link to="/userCenter">登录商家中心</nuxt-link></dd>
+            <dd><nuxt-link to="/coop">我想合作</nuxt-link></dd>
+            <dd><nuxt-link to="/wap">免费开店</nuxt-link></dd>
+            <dd><nuxt-link to="/kaipiao">商家申请开票</nuxt-link></dd>
+          </dl>
+        </li>
+        <li class="list site">
+          <nuxt-link to="/site">网站导航</nuxt-link>
+          <div class="subContainer">
+             <dl class="hotel">
+               <dt>酒店旅游</dt>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+             </dl>
+             <dl class="food">
+               <dt>吃美食</dt>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+             </dl>
+             <dl class="movie">
+               <dt>看电影</dt>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+               <dd>国际旅游</dd>
+             </dl>
+             <dl class="app">
+               <dt>APP</dt>
+               <dd>
+                 <a href="https://i.meituan.com/mobile/down/meituan" target="_blank"><img class="appicon" src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png" title="美团app" alt="美团app"></a>
+               </dd>
+               <dd>
+                 <a href="https://i.meituan.com/mobile/down/meituan" target="_blank"><img class="appicon" src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png" title="美团app" alt="美团app"></a>
+               </dd>
+               <dd>
+                 <a href="https://i.meituan.com/mobile/down/meituan" target="_blank"><img class="appicon" src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/appicons/meituan.png" title="美团app" alt="美团app"></a>
+               </dd>
+
+             </dl>
+          </div>
+        </li>
+      </ul>
     </div>
-  </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+
 
 export default {
-  components: {
-    Logo
-  }
+
 }
 </script>
 
 <style >
 
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
---
+++ components/public/header/searchbar.vue
@@ -0,0 +1,112 @@
+<template>
+   <div class="search-panel">
+     <el-row class="m-header-searchbar">
+       <el-col
+      :span="3"
+      class="left"
+       >
+         <img src="//s0.meituan.net/bs/fe-web-meituan/e5eeaef/img/logo.png" alt="美团">
+       </el-col>
+       <el-col
+         :span="15"
+         class="center"
+       >
+         <div class="wrapper">
+            <el-input
+              v-model="search"
+              @input="input"
+              placeholder="搜索商家和地点"
+             @focus="focus"
+              @blur="blur"
+            ></el-input>
+           <button class="el-button el-button--primary">
+             <i class="el-icon-search"></i>
+           </button>
+             <dl class="hotPlace"  v-if="isHotPlace" >
+                <dt>热门搜索</dt>
+                 <dd v-for="(item,idx) in hotPlace ":key="idex">{{item}}</dd>
+             </dl>
+
+           <dl class="searchList"  v-if="isSearchList"  >
+             <dd v-for="(item,idx) in searchList ":key="idex">{{item}}</dd>
+           </dl>
+         </div>
+         <p class="suggset">
+           <a href="#">故宫博物院</a>
+           <a href="#">故宫博物院</a>
+           <a href="#">故宫博物院</a>
+           <a href="#">故宫博物院</a>
+         </p>
+         <ul class="nav">
+            <li>
+              <nuxt-link to="/" class="takeout">
+                美团外卖
+              </nuxt-link>
+            </li>
+            <li>
+              <nuxt-link to="/" class="move">
+                猫眼电影
+              </nuxt-link>
+            </li>
+         </ul>
+         </el-col>
+       <el-col
+         :span ="6"
+       class="right"
+       >
+         <ul class="security">
+           <li><i class="refund"></i>
+            <p class="txt">随时退</p>
+           </li>
+           <li><i class="single"></i>
+            <p class="txt">不满意面单</p>
+           </li>
+           <li><i class="overdue"></i>
+            <p class="txt">过期退</p>
+           </li>
+         </ul>
+
+       </el-col>
+     </el-row>
+   </div>
+</template>
+
+<script>
+
+export default {
+    data(){
+        return{
+            search:'',
+            isFocus:false,
+          hotPlace:['火锅','拿你'],
+          searchList:['南极','背景','故宫']
+        }
+    },
+  computed:{
+        isHotPlace:function(){
+            return this.isFocus && !this.search
+        },
+        isSearchList:function(){
+                 return this.isFocus&&this.search
+        }
+  },
+  methods:{
+        focus:function(){
+            this.isFocus=true;
+        },
+        blur:function(){
+            let  self =this;
+            setTimeout(function(){
+                self.isFocus=false
+            },200)
+        },
+    input:function(){
+
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
