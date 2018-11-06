3b8a0a493df1a41db01c0252d749fe82f0ac5361
ex_gaojh1 <jiahao.gao@partner.midea.com>
2018-11-06 15:18:43 +0800

    1.引入babel 2. 编写header组件

---
+++ components/public/header/geo.vue
@@ -0,0 +1,21 @@
+<template>
+   <div class="m-geo">
+     <i class="el-icon-location">
+       北京市
+        <nuxt-link class="changeCity" to="/changeCity">切换城市</nuxt-link>
+       [香河 浪凡 田径]
+     </i>
+   </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style >
+
+</style>
